547bafb69e5681b73a71c4aa4554a3c0b34fd128
Felipe Suarez <felipesuarez1525@gmail.com>
2022-07-18 23:32:29 -0300

    new env

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -5,9 +5,9 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    authDomain: "suarez-ecommerce.firebaseapp.com",
     projectId: "suarez-ecommerce",
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    storageBucket: "suarez-ecommerce.appspot.com",
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
 };
